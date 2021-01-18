bdb6836b1f12de5b3d6026018357f3a3838d3546
Lenny Kean <lennykean@gmail.com>
2021-01-17 22:14:12 -0600

    Remove local main file

--- deps.ts
+++
@@ -1,4 +0,0 @@
-export { Request, Response } from "https://deno.land/x/oak/mod.ts";
-export * from "./momentum-di/mod.ts";
-export * from "./momentum-core/mod.ts";
-export * from "./momentum-oak/mod.ts";
