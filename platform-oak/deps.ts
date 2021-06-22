233e2caf5c2306664d52c1e129766f4271689597
Lenny Kean <lennykean@gmail.com>
2021-06-21 21:08:39 -0500

    fix(oak): lock oak version

--- platform-oak/deps.ts
+++ platform-oak/deps.ts
@@ -1,9 +1,13 @@
-export { Application, helpers, Router } from "https://deno.land/x/oak/mod.ts";
+export {
+  Application,
+  helpers,
+  Router,
+} from "https://deno.land/x/oak@v6.5.1/mod.ts";
 export type {
   FormDataBody,
   ListenOptions,
   RouterContext,
-} from "https://deno.land/x/oak/mod.ts";
+} from "https://deno.land/x/oak@v6.5.1/mod.ts";
 export * from "../core/mod.ts";
 export * from "../core/controller-metadata.ts";
 export * from "../di/mod.ts";
